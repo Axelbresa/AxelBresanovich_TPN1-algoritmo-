74ae92313ff91bb2f9da52445e8d18e75ddb687b
AxelBresa <bresanovichaxel43@gmail.com>
2024-04-02 17:59:08 -0300

    se corrigio el codigo del ejercicio 2 agregando restriciones y comentarios explicando el codigo

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,19 +1,18 @@
-# Se te dan todos los números entre 1 a “n” excepto uno. Tu tarea es encontrar el número que
-# falta.
-
-# Input:
-# El primer parámetro contiene la cantidad de elementos del array.
-# El segundo parámetro contiene “n” números. Cada número es único y está entre 1 y n
-# (inclusive).
-# Output:
-# Retornar el número que falta.
-
-def encontrar_faltante(cantidad_elementos, numeros):
+def encontrar_faltante(n, numeros):
+    #creamos un restriccion que si n no es mayor a 0 no continuara la secuencia
+    if n <= 0:
+        raise ValueError("El valor de n debe ser mayor a 0")
+    #numero ingresados
     numeros = [1, 2, 4, 5]  
-    suma_total = sum(range(1, cantidad_elementos + 1))
+    #suma el total de los numeros que te deberia dar
+    suma_total = sum(range(1, n + 1))
+    #suma de los numeros ingresados
     suma_dada = sum(numeros)
+    #resta la suma total con las suma de los numeros ingresados para obtener el faltante
     numero_faltante = suma_total - suma_dada
-    print(numero_faltante)
+    #imprimimos el valor del numero faltante
+    print("numero faltante: ", numero_faltante)
     return numero_faltante
 
+#caso de prueba
 assert encontrar_faltante(5, [1, 2, 4, 5]) == 3, "Error en el caso de prueba"
